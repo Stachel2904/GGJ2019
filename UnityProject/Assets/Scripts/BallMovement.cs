ff33c669c30d4fa95578171ff0d41dee4dff8433
Devin <devin97@gmx.de>
2019-01-25 22:32:24 +0100

    Balanced Movement

--- UnityProject/Assets/Scripts/BallMovement.cs
+++ UnityProject/Assets/Scripts/BallMovement.cs
@@ -18,15 +18,12 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = cam.transform.TransformDirection(new Vector3(moveHorizontal, 0.0f, moveVertical));
 
-        rb.AddForce(movement * speed);
-        
-        
+        float factor = Vector3.Angle(rb.velocity, movement * speed) / 180 * speed;
+        rb.velocity += movement * (speed + factor);
     }
 }
