6effc28d1e564c4906eafa015a9d81e61f291847
Sascha <suitatsu@gmx.de>
2019-01-25 17:40:22 +0100

    Camera Behavior

--- UnityProject/Assets/Scripts/CameraBehaviour.cs
+++ UnityProject/Assets/Scripts/CameraBehaviour.cs
@@ -13,6 +13,6 @@ public class CameraBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       this.gameObject.transform.Translate(Vector3.forward);
     }
 }
